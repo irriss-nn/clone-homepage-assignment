764765e52c52a93caff7e2c5bc3c95ac9052b1cb
iris <77365934+irriss-nn@users.noreply.github.com>
2023-09-07 22:58:50 -0600

    add comment

--- GH-Homepage-Clone/app/page.tsx
+++ GH-Homepage-Clone/app/page.tsx
@@ -1,3 +1,4 @@
+// Required imports
 import Head from 'next/head';
 import Navbar from './components/Navbar/Navbar';
 import FirstPage from './components/FirstPart/FirstPage';
@@ -8,48 +9,80 @@ import Security from './components/FourthPart/SecurityPage';
 import Globe from './components/FifthPart/Globe';
 import Footer from './components/Foot/Footer';
 
+// Home component
 export default function Home() {
   
   return (
     <>
-    <Head>
-      <title>GitHub: Where the world builds software · GitHub</title>
-    </Head>
-    <div className=' '>
-      <div className='relative z-50'>
-        <div className='absolute'>
-        <Navbar/>
-        </div>
-      </div>
-      <div>
-        <div className='overflow-hidden'>
-          <div className='relative'>
-            <img className='absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image' width='4377' src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp" alt="" />
+      {/* Head section for setting up the title */}
+      <Head>
+        <title>GitHub: Where the world builds software · GitHub</title>
+      </Head>
+
+      {/* Main Content Wrapper */}
+      <div className=' '>
+        
+        {/* Navbar section */}
+        <div className='relative z-50'>
+          <div className='absolute'>
+            <Navbar />
           </div>
+        </div>
+        
+        {/* Main Content Area */}
+        <div className='overflow-hidden'>
+          
+          {/* Background image section */}
+          <div className='relative'>
+            <img
+              className='absolute top-0 transition ease-in duration-200 max-xl:right-[-1050px] xl:right-[-970px] -z-30 image'
+              width='4377'
+              src="https://github.githubassets.com/images/modules/site/home-campaign/hero-bg.webp"
+              alt="Background image for GitHub home page"
+            />
+          </div>
+
+          {/* First page component */}
           <div className='hero-section px-3 '>
             <FirstPage/>
           </div>
-          <FirstPart/>
+
+          {/* More content components */}
+          <FirstPart />
+          
           <div id='productivity' className='home-campaign-productivity px-4 pt-8 overflow-hidden'>
-            <Productivity/>
+            <Productivity />
           </div>
+          
           <div id='collaboration' className='home-campaign-productivity px-4 pt-8  overflow-hidden'>
-            <Collaboration/>
+            <Collaboration />
           </div>
+
           <div id='security' className='home-campaign-productivity px-4 pt-8 pb-16 overflow-hidden'>
-            <Security/>
+            <Security />
           </div>
-        <Globe/>
+
+          {/* Globe component */}
+          <Globe />
+
+          {/* Footnotes */}
           <div className='max-w-[1280px] mx-auto relative z-[2]  px-5'>
             <ul className='lg:w-9/12 text-[14px] pt-16 text-[#7d8590] space-y-1'>
-              <li><sup id="footnote-1">1</sup> The Total Economic Impact™ Of GitHub Enterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers.</li>
-              <li><sup id="footnote-2">2</sup> GitHub, Octoverse 2022 The state of open source software.</li>
+              <li>
+                <sup id="footnote-1">1</sup> The Total Economic Impact™ Of GitHub Enterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers.
+              </li>
+              <li>
+                <sup id="footnote-2">2</sup> GitHub, Octoverse 2022 The state of open source software.
+              </li>
             </ul>
           </div>
+
         </div>
+
+        {/* Footer component */}
+        <Footer />
+
       </div>
-      <Footer/>
-    </div>
     </>
   );
 }
